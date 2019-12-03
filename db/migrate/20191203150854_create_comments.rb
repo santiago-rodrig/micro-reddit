3af1ada266cc883d287a2f0902307dfc7aa66efc
Santiago <santo1996.29@gmail.com>
2019-12-03 11:11:10 -0400

    comment reference can be null, migration

--- db/migrate/20191203150854_create_comments.rb
+++ db/migrate/20191203150854_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.string :author
       t.references :link, null: false, foreign_key: true
-      t.references :comment, null: false, foreign_key: true
+      t.references :comment, null: true, foreign_key: true
 
       t.timestamps
     end
