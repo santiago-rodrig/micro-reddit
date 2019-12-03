669f0de2b366a35f15445769422266038f7169aa
Santiago <santo1996.29@gmail.com>
2019-12-03 11:30:44 -0400

    add email to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   validates :name, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, format: {
+    with: VALID_EMAIL_REGEX }, length: { maximum: 255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_151431) do
+ActiveRecord::Schema.define(version: 2019_12_03_152036) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_151431) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "comments", "comments"
